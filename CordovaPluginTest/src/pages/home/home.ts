75ceb34b3df140840f7ab16cf2873a9aa050fab7
liujf <cnliujianfei@163.com>
2017-08-30 13:48:12 +0800

    add cordova-plugin-ble-secret

--- CordovaPluginTest/src/pages/home/home.ts
+++ CordovaPluginTest/src/pages/home/home.ts
@@ -2,15 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { Platform } from 'ionic-angular';
-declare var window: any;
-// interface Window {
-//   sqlitePlugin: any;
-//   secret: any;
-// }
-// declare var window: Window;
-declare var cordova: any;
-declare var window: any;
-// declare var cordova: any;
+declare var ble_secret: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -18,62 +10,39 @@ declare var window: any;
 export class HomePage {
   submitted: boolean = false;
   beforeText: string;
-  constructor(public navCtrl: NavController, public platForm: Platform) {
-    // platForm.ready().then(() => {
-    //   if (cordova && cordova.InAppBrowser) {
-    //     window.open = cordova.InAppBrowser.open;
-    //   }
-    // });
-  }
-  // getIpAddress() {
-  //   alert('getIpAddress');
-  //   if ('undefined' != typeof networkinterface) {
-  //     networkinterface.getIPAddress(function (ip) {
-  //       alert(ip);
-  //     });
-  //   }
-  // }
-  submit(form: NgForm) {
-    this.submitted = true;
-    // var db = (<any>window).sqlitePlugin.openDatabase({name: "my.db"});
-    if (form.valid) {
-      // this.getIpAddress();
-      // alert(this.beforeText)
-      this.beforeText = '';
-      this.submitted = false;
-      // this.platForm.ready().then(
-      //   function doSomeThing() {
-      //     (<any>window).secret.coolMethod('hello');
-      //   }
-      // );
-      // this.platForm.ready().then(
-      //   () => {
-      //     console.log("MyApp::constructor platform.ready");
-      //     cordova.plugins.backgroundMode.setDefaults({
-      //       title: 'My App Name',
-      //       text: 'Active in background...'});
-      //     cordova.plugins.backgroundMode.enable();
-      //   }
-      // );
-      // cordova.plugins.
-      // if (this.platForm){
-      //   alert(this.platForm.version());
-      // }
-      // for(var i=0;i<window.plugins.count;i++) {
-      //   alert(window.plugins[i].name);
-      // }
-      // if (!window.networkinterface) {
-      //   alert('networkinterface is not avialable');
-      // }
-      window.plugins.secret.coolMethod('hello');
-      // if (!window.BleSecret) {
-      //   alert('ble_secret is not avialable');
-      // }
+  afterText: string;
 
-      // if (!(<any>window).ble_secret) {
-      //   alert('ble_secret is not avialable');
-      // }
-      // ble_secret.coolMethod('hello');
+
+
+
+  constructor(public navCtrl: NavController, public platForm: Platform) {
+  }
+
+  submit(form: NgForm) {
+    var temp = this;
+    this.submitted = true;
+    if (form.valid) {
+      if (this.beforeText == '' && this.afterText == '') {
+        return;
+      }
+      if (this.beforeText != '')
+      {
+        ble_secret.encode(this.beforeText,  function(success) {
+          temp.afterText = success;
+        }, function (error) {
+          alert(error);
+        });
+        this.beforeText = '';
+      } else if (this.afterText != '')
+      {
+        ble_secret.secret.decode(this.afterText, function (success) {
+          temp.beforeText = success;
+        }, function (error) {
+          alert(error);
+        });
+        this.afterText = '';
+      }
+      this.submitted = false;
     }
   }
 }
